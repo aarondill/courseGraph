33fffe624498534fc6ccf3351b0b8659c6db2e79
Aaron Dill <aaronsacks2006@gmail.com>
2025-10-02 22:21:12 -0500

    feat: add generated line to plan

--- src/plan.ts
+++ src/plan.ts
@@ -1,3 +1,4 @@
+import packageJson from "../package.json" with { type: "json" };
 import { json } from "./input.ts";
 function printPlan(all: Record<string, string[]>) {
   for (const [semester, courses] of Object.entries(all)) {
@@ -8,6 +9,9 @@ function printPlan(all: Record<string, string[]>) {
   }
 }
 
+console.log(`# ${json.degree}`);
+console.log(`_Generated using [CourseGraph](${packageJson.repository})_`);
+console.log(""); // Empty line
 printPlan(json.taken);
 console.log(`\n----------\n`); // Separate taken from future
 printPlan(json.future);
